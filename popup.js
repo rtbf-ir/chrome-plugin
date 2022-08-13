fcd0b40df375fe266e25be2e148cd6ef4f32f53a
Amir Shokri <amirsh.nll@gmail.com>
2022-08-13 23:25:48 +0430

    change some files

--- popup.js
+++ popup.js
@@ -1,71 +1,85 @@
-const DATA_URL = 'https://rtbf.ir/data/data.json';
+const DATA_URL = "https://rtbf.ir/data/data.json";
 
 const DIFFICULTY_COLORS = {
-    "impossible-label": "#000000",
-    "hard-label": "#a32100",
-    "easy-label": "#129141",
-    "medium-label": "#ffa800"
-}
+  "impossible-label": "#000000",
+  "hard-label": "#a32100",
+  "easy-label": "#129141",
+  "medium-label": "#ffa800",
+};
 
 const DIFFICULTY_IMAGES = {
-    "impossible-label": '/assets/images/d_impossible_logo.png',
-    "hard-label": '/assets/images/d_hard_logo.png',
-    "easy-label": '/assets/images/d_easy_logo.png',
-    "medium-label": '/assets/images/d_medium_logo.png',
-}
+  "impossible-label": "/assets/images/d_impossible_logo.png",
+  "hard-label": "/assets/images/d_hard_logo.png",
+  "easy-label": "/assets/images/d_easy_logo.png",
+  "medium-label": "/assets/images/d_medium_logo.png",
+};
 
 function changeIcon(difficulty, tabId) {
-    chrome.action.setIcon({path: DIFFICULTY_IMAGES[difficulty], tabId})
+  chrome.action.setIcon({ path: DIFFICULTY_IMAGES[difficulty], tabId });
 }
 
-function hideSpinner(){
-    document.querySelector('.spinner-container').style.display = 'none'
+function hideSpinner() {
+  document.querySelector(".spinner-container").style.display = "none";
 }
 
 function showDifficulty(item, tabId) {
-    const {difficulty: difficultyLabel, keytype: difficulty, info, deleteurl} = item
+  const {
+    difficulty: difficultyLabel,
+    keytype: difficulty,
+    info,
+    deleteurl,
+  } = item;
 
-    changeIcon(difficulty, tabId)
+  changeIcon(difficulty, tabId);
 
-    document.querySelector('.difficulty-text').innerText = difficultyLabel
-    document.querySelector('.difficulty-text').style.backgroundColor = DIFFICULTY_COLORS[difficulty]
-    document.querySelector('.difficulty-info')
-        .insertAdjacentHTML('afterbegin', info);
+  document.querySelector(".difficulty-text").innerText = difficultyLabel;
+  document.querySelector(".difficulty-text").style.backgroundColor =
+    DIFFICULTY_COLORS[difficulty];
+  document
+    .querySelector(".difficulty-info")
+    .insertAdjacentHTML("afterbegin", info);
 
-    if(deleteurl !== '#') {
-        document.querySelector('.remove-button').style.display = 'block'
-        document.querySelector('.remove-button').href = deleteurl
-    }
+  if (deleteurl !== "#") {
+    document.querySelector(".remove-button").style.display = "block";
+    document.querySelector(".remove-button").href = deleteurl;
+  }
 
-    hideSpinner()
+  hideSpinner();
 
-    document.querySelector('.difficulty-container').style.display = 'flex'
+  document.querySelector(".difficulty-container").style.display = "flex";
 }
 
 function showNotSupported() {
-    hideSpinner()
-    document.querySelector('.not-supported-container').style.display = 'block'
+  hideSpinner();
+  document.querySelector(".not-supported-container").style.display = "block";
 }
 
-chrome?.tabs?.query({
+chrome?.tabs?.query(
+  {
     active: true,
-    lastFocusedWindow: true
-}, function (tabs) {
+    lastFocusedWindow: true,
+  },
+  function (tabs) {
     const currentUrl = tabs[0].url;
     const tabId = tabs[0].id;
 
     fetch(DATA_URL)
-        .then((response) => response.json())
-        .then((websites) => {
-            for (let item of websites) {
-                if (currentUrl.includes(item.website)) {
-                    showDifficulty(item, tabId);
-                    return;
-                }
-            }
+      .then((response) => response.json())
+      .then((websites) => {
+        for (let item of websites) {
+          if (currentUrl.includes(item.website)) {
+            showDifficulty(item, tabId);
+            return;
+          }
+        }
 
-            showNotSupported();
-        }).catch(error => {
-            console.error('There has been a problem with fetch websites data:', error);
-        });
-});
+        showNotSupported();
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with fetch websites data:",
+          error
+        );
+      });
+  }
+);
