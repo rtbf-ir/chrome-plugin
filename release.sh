e593c6730f1fe438d029adbe090398f3611afa25
Amir Shokri <amirsh.nll@gmail.com>
2025-10-20 12:04:08 +0330

    add donation link

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 find . -type f -name ".DS_Store" -delete
 zip -r chrome.zip . --exclude ".git/*" --exclude "release.sh" --exclude .DS_Store
-mv chrome.zip ~/Downloads/chrome.zip
+mv chrome.zip ~/Downloads/rtbf-chrome.zip
